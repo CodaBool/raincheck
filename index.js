b974cfb839fdf2816c1c4a0881aa77d61d46949f
codabool <codabool@pm.me>
2021-06-19 23:28:01 -0400

    add details to message

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const axios = require('axios');
   const sevenDays = 604800
 
   let data = null
-  await axios.get(`https://history.openweathermap.org/data/2.5/history/city?lat=${process.env.LAT}&lon=${process.env.LON}&type=hour&start=${currentTime - sevenDays}&end=${currentTime}&appid=${process.env.key1}`)
+  await axios.get(`https://history.openweathermap.org/data/2.5/history/city?lat=${process.env.LAT}&lon=${process.env.LON}&type=hour&start=${currentTime - sevenDays}&end=${currentTime}&appid=${process.env.API_KEY}`)
     .then(res => data = res.data)
     .catch(console.log)
 
@@ -26,7 +26,6 @@ const axios = require('axios');
   })
 
   console.log('extractedData', extractedData)
-  console.log('it rained', totalRain, 'inches this week')
 
   const week = [0,1,2,3,4,5,6]
   const rainData = {
@@ -48,17 +47,19 @@ const axios = require('axios');
     }
   })
 
+  let body = 'it rained ' + String(totalRain) + ' inches this week'
+
   for (const key in rainData) {
-    if (key === '0') console.log('Sunday it rained', rainData[key], 'inches')
-    if (key === '1') console.log('Monday it rained', rainData[key], 'inches')
-    if (key === '2') console.log('Tuesday it rained', rainData[key], 'inches')
-    if (key === '3') console.log('Wednesday it rained', rainData[key], 'inches')
-    if (key === '4') console.log('Thursday it rained', rainData[key], 'inches')
-    if (key === '5') console.log('Friday it rained', rainData[key], 'inches')
-    if (key === '6') console.log('Saturday it rained', rainData[key], 'inches')
+    if (key === '0') body += `\nSunday it rained ${rainData[key]} inches`
+    if (key === '1') body += `\nMonday it rained ${rainData[key]} inches`
+    if (key === '2') body += `\nTuesday it rained ${rainData[key]} inches`
+    if (key === '3') body += `\nWednesday it rained ${rainData[key]} inches`
+    if (key === '4') body += `\nThursday it rained ${rainData[key]} inches`
+    if (key === '5') body += `\nFriday it rained ${rainData[key]} inches`
+    if (key === '6') body += `\nSaturday it rained ${rainData[key]} inches`
   }
 
-  const body = 'it rained ' + String(totalRain) + ' inches this week'
+  console.log('sending message\n' + body)
 
   client.messages
     .create({
